f5649c27136f264ac33689f8af07945f0a106c8c
Tejas Kulkarni <tejaskulkarni2801@gmail.com>
2023-09-24 01:30:50 +0530

    Commited

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,40 @@
 package com.company;
 
+class loadGameClass{
+    char xos[][];
+
+    // constructor to initialize an object
+    loadGameClass(){
+        xos = new char[3][3];
+        initBoard();
+    }
+
+    public void initBoard(){
+        for(int i=0; i<xos.length; i++){
+            for(int j=0; j<xos[i].length; j++){
+                xos[i][j] = ' ';
+            }
+        }
+    }
+
+    public void loadBoard(){
+        System.out.println("---------------");
+        for(int i=0; i<xos.length; i++){
+            System.out.print(" | ");
+            for(int j=0; j<xos[i].length; j++){
+                System.out.print(xos[i][j] + " | ");
+            }
+            System.out.println("");
+            System.out.println("---------------");
+        }
+    }
+}
+
 public class Main {
 
     public static void main(String args[]) {
 	// write your code here
+        loadGameClass ref = new loadGameClass();
+        ref.loadBoard();
     }
 }
